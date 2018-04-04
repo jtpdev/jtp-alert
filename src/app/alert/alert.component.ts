dbfd83b4fe2d8c6f2759abe8663e7cdc7ad159d1
Jimmy Porto <ireturnerd@gmail.com>
2018-04-04 14:56:17 -0300

    Updated css

--- src/app/alert/alert.component.ts
+++ src/app/alert/alert.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'jtp-alert',
   templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.css', '../../../node_modules/ionicons/css/ionicons.css']
+  styleUrls: ['./alert.component.css', './ionicons/css/ionicons.css']
 })
 export class AlertComponent implements OnInit {
 
