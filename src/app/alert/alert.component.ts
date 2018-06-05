97c4773e456e8ee4059d2a5b09e417deb5e7b731
Jimmy Porto <ireturnerd@gmail.com>
2018-06-05 10:29:30 -0300

    Solved #1

--- src/app/alert/alert.component.ts
+++ src/app/alert/alert.component.ts
@@ -1,75 +1,78 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'jtp-alert',
-  templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.scss']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AlertComponent implements OnInit {
+export class AppComponent {
+  title = 'app';
 
-  @Input() data: any;
-  shown = false;
-  color = '#03A9F4';
-  constructor() { }
-
-  ngOnInit() {
-    this.init();
-  }
-
-  init() {
-    if (this.data) {
-      if (this.data.type) {
-        switch (this.data.type) {
-          case 'success':
-            this.color = '#72BB53';
-            break;
-          case 'error':
-            this.color = '#FF3823';
-            break;
-        }
-        document.getElementById('title').style.backgroundColor = this.color;
-        document.getElementById('content').style.borderColor = this.color;
-        document.getElementById('footer').style.borderColor = this.color;
+  dataS = {
+    title: 'Test title',
+    message: 'Message for user',
+    type: 'success',
+    btns: [
+      {
+        name: 'OK',
+        function: () => {
+          console.log('Ok button works');
+        },
+        order: 1,
+        close: true,
+        main: true
+      },
+      {
+        name: 'Cancel',
+        close: false
       }
-      if (this.data.btns) {
-        this.data.btns = this.data.btns.sort((b1, b2) => {
-          return (b1.order ? b1.order : 0) - (b2.order ? b2.order : 0);
-        });
+    ]
+  };
+
+  dataA = {
+    title: 'Test title',
+    message: 'Message for user',
+    type: 'alert',
+    btns: [
+      {
+        name: 'OK',
+        function: () => {
+          console.log('Ok button works');
+        },
+        order: 1,
+        close: true,
+        main: true
+      },
+      {
+        name: 'Cancel',
+        close: false
       }
-    }
-  }
+    ]
+  };
 
-  show() {
-    this.init();
-    this.shown = true;
-  }
+  dataE = {
+    title: 'Test title',
+    message: 'Message for user',
+    type: 'error',
+    btns: [
+      {
+        name: 'OK',
+        function: () => {
+          console.log('Ok button works');
+        },
+        order: 1,
+        close: true,
+        main: true
+      },
+      {
+        name: 'Cancel',
+        close: false
+      }
+    ]
+  };
 
-  hide() {
-    this.shown = false;
-  }
-
-  call(fnc, close) {
-    if (close != false) {
-      this.hide();
-    }
-    if (fnc) fnc();
-  }
-
-  style(main) {
-    let btnClass = {
-      'color': this.color,
-      'background-color': 'white',
-      'border': `1px solid ${this.color}`,
-      'border-color': this.color,
-      'margin-left': '5px'
-    }
-
-    if (main) {
-      btnClass['background-color'] = this.color;
-      btnClass['color'] = 'white';
-    }
-
-    return btnClass;
+  show(alert){
+    alert.show();
   }
 
 }
